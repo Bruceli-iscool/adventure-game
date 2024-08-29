d165a24e60586f1b121bb3aaf804ad3f94844665
Bruce Li <isanoob1897@outlook.com>
2024-08-28 18:00:37 -0700

    add

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -1,13 +1,12 @@
 package dev.desktop;
 import java.util.Scanner;
-import dev.desktop.*;
 public class App {
     public static void main(String args[]) {
         System.out.println("Welcome to Adventure Game");
         System.out.println();
         String ans = ask("Create a new character? Y, n");
         if ("Y".equals(ans.strip())) {
-            characterCreator();
+            characterCreator c = new characterCreator();
         }
     }
     @SuppressWarnings("resource")
--- app/src/main/java/dev/desktop/characterCreator.java
+++ app/src/main/java/dev/desktop/characterCreator.java
@@ -1,16 +1,22 @@
 package dev.desktop;
-public class characterCreator {
+class characterCreator {
     int attack, defence, health, stanima;
-    public void characterCreator() {
+    public characterCreator() {
         System.out.println("You have 20 points to spend on 4 abilities. Each stat has a 10 point limit");
         int points = 20;
-        attack = Integer.getInteger(App.ask("Enter Attack: "));
+        attack = Integer.parseInt(App.ask("Enter Attack: "));
         if (attack > 10) {
+            // if bigger than 10, just make attack 10
             attack = 10;
             points = points - 10;
         } else {
             points = points - attack;
         }
-        
+        System.out.println("You now have: " + points + " points");
+        defence = Integer.parseInt(App.ask("Enter defence: "));
+        if (defence > 10) {
+            defence = 10;
+            points = points - defence;
+        }
     }
 }
