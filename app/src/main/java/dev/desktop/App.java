c5a9f3941a7372e34c14c7ee4bbae53657e6fb9b
Bruce Li <isanoob1897@outlook.com>
2024-09-08 13:42:46 -0700

    add and fix

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -1,13 +1,14 @@
 package dev.desktop;
 import java.util.Scanner;
 public class App {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
         System.out.println("Welcome to Adventure Game");
         System.out.println();
         String ans = ask("Create a new character? Y, n");
         if ("Y".equals(ans.strip())) {
             @SuppressWarnings("unused")
             characterCreator c = new characterCreator();
+            c.create();
         }
     }
     @SuppressWarnings("resource")
--- app/src/main/java/dev/desktop/character.java
+++ app/src/main/java/dev/desktop/character.java
@@ -7,25 +7,37 @@ class character {
         health = h;
         stamina = s;
     }
-    public void start() {
+    public void start() throws InterruptedException {
         System.out.println("Welcome explorer! The emperor wants to see you!\nChoose a ability to train in\n1 for attack, 2 for defence, 3 for health, 4 for stamina");
         String ans = App.ask(">");
         if ("1".equals(ans.strip())) {
             attack += 1;
         }
-        if ("2".equals(ans.strip())) {
+        else if ("2".equals(ans.strip())) {
             defence += 1;
         }
-        if ("3".equals(ans.strip())) {
+        else if ("3".equals(ans.strip())) {
             health += 1;
         }
-        if ("4".equals(ans.strip())) {
+        else if ("4".equals(ans.strip())) {
             stamina += 1;
         }
         else {
             System.out.println("Not a valid option!");
             start();
         }
+        System.out.println("""
+                           Your stats are: 
+                           attack: """ + attack + "\ndefence: " + defence + "\nhealth: " + health + "\nstamina: " + stamina);
+    
+        System.out.println("Adventurer, the. kingdom is crumbling!\nThe army of death has aprroached, destorying the southern lands\nI will not live, deliver to the heir the Kings Crown!");
+        Thread.sleep(1000);
+        System.out.println(">\nYou recieved the Kings Crown."); 
+        pathOne();
     }
+    public void pathOne() {
+
+    }
+
 }
 
