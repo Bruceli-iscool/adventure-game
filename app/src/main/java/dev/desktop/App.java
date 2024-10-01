4a43fce50bd735e61e354a57e90bfc2a6b1510a7
Bruce Li <isanoob1897@outlook.com>
2024-10-01 23:45:37 +0000

    add

--- app/src/main/java/dev/desktop/App.java
+++ app/src/main/java/dev/desktop/App.java
@@ -1,7 +1,7 @@
 package dev.desktop;
 import java.util.Scanner;
 public class App {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws InterruptedException {
         System.out.println("Welcome to Adventure-Game\n================================================================");
         System.out.println("""   
    ____                     
