f8b3d475e3953e9c1476698db93ca5bbaa4b1054
Bruce Li <isanoob1897@outlook.com>
2024-10-04 17:07:43 -0700

    fix

--- app/src/main/java/dev/desktop/Orc.java
+++ app/src/main/java/dev/desktop/Orc.java
@@ -2,12 +2,17 @@ package dev.desktop;
 
 // code for the orc enemy
 public class Orc {
+    int health;
+    int stamina;
+    int attack;
+    int defence;
+    boolean turn = true;
     public Orc(int h, int s, int a, int d) {
-        int health = h;
-        int stamina = s;
-        int attack = a;
-        int defence = d;
-        boolean turn = true;
+health = h;
+stamina = s;
+attack = a;
+defence = d;
+
     }
     public boolean attacked(int damage) {
         health = defence - damage;
