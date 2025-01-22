01bc2b05418ac78f5637f2c9383b21b682ce2dd0
Bruce Li <isanoob1897@outlook.com>
2025-01-21 20:53:31 -0800

    add groovy files

---
+++ app/src/main/groovy/dev/desktop/quests1.groovy
@@ -0,0 +1,20 @@
+package dev.desktop
+
+class quests1 {
+    // do another 2 quests in groovy, then create a new java class for quests
+    // and rotate between them
+    def attack, defence, health, stamina, gold
+    quests1(def a, def d, def h, def s, def g) {
+        attack = a
+        defence = d
+        health = h
+        stamina = s
+        gold = g
+    }
+    void quest5() throws InterruptedException, IOException {
+        // code for quest 5 migrated to groovy
+        // town with two quest options
+        // todo
+        println "=========================================================\nQuest 5\n====================================================================";
+    }
+}
