8cd756e3d73edf27f450fd0bbb8131c7f4e24ed9
Bruce Li <isanoob1897@outlook.com>
2025-01-22 20:13:54 +0000

    change pause function

--- app/src/main/groovy/dev/desktop/quests1.groovy
+++ app/src/main/groovy/dev/desktop/quests1.groovy
@@ -13,18 +13,18 @@ class quests1 {
     }
     void quest5() throws InterruptedException, IOException {
         println "=========================================================\nQuest 5\n====================================================================";
-        Thread.sleep(500);
+        sleep(500);
         println "You arrive at a town and see a guard, 1 to talk to him, 2 to ignore him.";
-        Thread.sleep(500);
+        sleep(500);
         def n = App.ask(">");
         if ("1".equals(n)) {
             println "You talk to the guard and he tells you about bandits in the area that have been attacking the town.";
-            Thread.sleep(500);
+            sleep(500);
             println "He offers you 100 gold to kill the bandit leader (Very Hard, +3 health), 1 to accept, 2 to decline.";
             n = App.ask(">");
             if ("1".equals(n)) {
                 println "You accept the quest and head out to find the bandit leader.";
-                Thread.sleep(500);
+                sleep(500);
                 App.save(attack, defence, health, stamina, 6, gold);
                 quest6();
             } else {
@@ -32,26 +32,26 @@ class quests1 {
             }
 
         }
-        Thread.sleep(500);
+        sleep(500);
         System.out.println("You walk further into the town and enter the bar.");
-        Thread.sleep(500);
+        sleep(500);
         println "The bar owner gives you a quest to find his lost sword in the Dungeons of Doom (+10 gold) (Hard, +3 health bonus)."
-        Thread.sleep(500);
+        sleep(500);
         println "1 to accept, 2 to decline.";
         n = App.ask(">");
         if ("1".equals(n)) {
             println "You accept the quest and head out to find the sword.";
-            Thread.sleep(500);
+            sleep(500);
             App.save(attack, defence, health, stamina, 7, gold);
             quest7();
         } else {
             println "You decline the quest and continue walking.";
-            Thread.sleep(500);
+            sleep(500);
             println "Another guard approaches you and tells you about the king's missing crown. (+10 gold) (Easy, +1 health bonus), 1 to accept, 2 to decline and go on the Bar owner's quest.";
             n = App.ask(">");
             if ("1".equals(n)) {
                 println "You accept the quest and head out to find the crown.";
-                Thread.sleep(500);
+                sleep(500);
                 App.save(attack, defence, health, stamina, 8, gold);
                 quest8();
             } else {
@@ -68,7 +68,7 @@ class quests1 {
         defence += 2;
         shop();
         println "You travel in the woods to reach the bandit leader's hideout.";
-        Thread.sleep(500);
+        sleep(500);
         
     }
     void quest7() throws InterruptedException, IOException {
