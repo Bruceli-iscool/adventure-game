e465af7c419823f94eacc660be831f3639650cd4
Bruce Li <isanoob1897@outlook.com>
2025-04-23 07:38:04 -0700

    start on lexer

--- app/src/main/java/dev/desktop/dsl.java
+++ app/src/main/java/dev/desktop/dsl.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class dsl extends character {
 	int attack, defence, health, stamina, gold;
+	ArrayList<String> n = new ArrayList<>();
 	List<String> content;
 	public dsl(int a, int d, int h, int s, int g, List<String> c) {
 		super(a, d, h, s, g);
@@ -17,34 +18,11 @@ public class dsl extends character {
 		gold = g;
 		content = c;
 	}
+	protected void lex() {
+
+	}
 	protected void parse() throws InterruptedException{
-<<<<<<< HEAD
-		// lex todo
-=======
-        boolean mode = false;
-		for (String n:content) {
-			n = n.trim();
-			if (n.contains("combat(")) {
-				String newN = n.replace("combat(", "");
-				try {
-					newN = newN.replace(")", "");
-					newN = newN.replace(";", "");
-					String[] arr = newN.split(",");
-					int[] arr2 = new int[3];
-					int index = 0;
-					for (String i:arr) {
-						int current = Integer.parseInt(i.trim());
-						arr2[index] = current;
-						index++;
-					}
-					combat(arr2[0], arr2[1], arr2[2], arr2[3]);
-				} catch (Exception e) {
-					System.err.println("Adv: An Error Occured: " + e);
-				}
-			}
-			
-		}
->>>>>>> 33e8c1b (add)
+
 
 	}	
 }
