45e8a21e256bf94728af70e46aed9ea4439480be
Bruce Li <isanoob1897@outlook.com>
2025-06-25 19:54:53 -0700

    Update dsl.java
    Signed-off-by: Bruce Li <isanoob1897@outlook.com>

--- app/src/main/java/dev/desktop/dsl.java
+++ app/src/main/java/dev/desktop/dsl.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class dsl extends character {
 	int attack, defence, health, stamina, gold;
-	ArrayList<String> n = new ArrayList<>();
+	ArrayList<String> n = new ArrayList<>(); 
 	List<String> content;
 	public dsl(int a, int d, int h, int s, int g, List<String> c) {
 		super(a, d, h, s, g);
